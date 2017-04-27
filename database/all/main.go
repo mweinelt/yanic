1fba6902ea7c8d72c561f17f6ed7435d589ac2b6
Martin Geno <geno+dev@fireorbit.de>
2017-04-27 21:09:46 +0200

    [TASK] database as socket output

--- database/all/main.go
+++ database/all/main.go
@@ -3,4 +3,5 @@ package all
 import (
 	_ "github.com/FreifunkBremen/yanic/database/influxdb"
 	_ "github.com/FreifunkBremen/yanic/database/logging"
+	_ "github.com/FreifunkBremen/yanic/database/socket"
 )
